d3d1ecb0a2292f0348154a091b20f145044cb624
maddadder <rleecharlie@gmail.com>
2023-09-27 17:51:49 -0700

    show historical generated files

--- server.py
+++ server.py
@@ -1,66 +1,55 @@
-import subprocess
 import os
-import uuid
-import shutil
-import glob
-import json
-import random
-import shutil
-from fastapi import FastAPI, File, UploadFile, HTTPException, Form
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
-import re
+from fastapi.templating import Jinja2Templates
 import torchaudio
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
-import uuid
 import time
 
 model = MusicGen.get_pretrained('facebook/musicgen-medium', device='cuda')
-model.set_generation_params(duration=12) 
+model.set_generation_params(duration=12)
 
 model_melody = MusicGen.get_pretrained('facebook/musicgen-melody', device='cuda')
-model_melody.set_generation_params(duration=16) 
+model_melody.set_generation_params(duration=16)
 
 app = FastAPI()
 
 # Serve static files (including HTML) from a directory named "static"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
 @app.post("/musicgen")
-def musicgen(text: str = Form(...), response_class=FileResponse):
+def musicgen(text: str = Form(...), response_class=HTMLResponse):
     try:
-        start = time.time()
-        wav = model.generate([text], progress=True)
-        wav = wav[0]
-        end = time.time()
-        print(f"Generation took {end - start} seconds")
+        generated_files = []
+        for i in range(3):  # Generate 3 audio samples
+            start = time.time()
+            wav = model.generate([text], progress=True)
+            wav = wav[0]
+            end = time.time()
+            print(f"Generation took {end - start} seconds")
 
-        save_path = f"/tmp/{uuid.uuid4()}"
+            save_path = f"static/{uuid.uuid4()}.wav"
+            generated_files.append(save_path)
 
-        print(f"Saving {save_path}")
-        audio_write(f'{save_path}', wav.cpu(), model.sample_rate, strategy="loudness")
+            print(f"Saving {save_path}")
+            audio_write(f'{save_path}', wav.cpu(), model.sample_rate, strategy="loudness")
 
-        save_path = save_path + ".wav"
-        return FileResponse(save_path, media_type="audio/wav")
+        return templates.TemplateResponse("results.html", {"request": text, "generated_files": generated_files})
     except Exception as e:
         print(f"An error occurred: {e}")
 
-@app.post("/melody")
-def melody(text: str = Form(...), response_class=FileResponse):
-    try:
-        start = time.time()
-        wav = model_melody.generate([text], progress=True)
-        wav = wav[0]
-        end = time.time()
-        print(f"Generation took {end - start} seconds")
+# HTML template to display results
+@app.get("/results", response_class=HTMLResponse)
+async def results(request: str, generated_files: list):
+    return {"request": request, "generated_files": generated_files}
 
-        save_path = f"/tmp/{uuid.uuid4()}"
-
-        print(f"Saving {save_path}")
-        audio_write(f'{save_path}', wav.cpu(), model_melody.sample_rate, strategy="loudness")
-
-        save_path = save_path + ".wav"
-        return FileResponse(save_path, media_type="audio/wav")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# Endpoint to list available generated files
+@app.get("/list_generated_files", response_class=HTMLResponse)
+async def list_generated_files():
+    generated_files = [f for f in os.listdir("static") if f.endswith(".wav")]
+    return templates.TemplateResponse("list_files.html", {"generated_files": generated_files})
