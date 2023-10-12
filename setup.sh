285cec597f32a28de548799693267359af57a861
maddadder <rleecharlie@gmail.com>
2023-10-12 14:44:40 -0700

    use cuda when installing pytorch

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@
 conda create --name audio python=3.9
 #conda deactivate
 conda activate audio
-pip install 'torch>=2.0'
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft
 conda install "ffmpeg<5" -c conda-forge
 pip install git+https://github.com/huggingface/transformers.git
+pip install chardet
+pip install numpy==1.25
 
 #run the following
+conda activate audio
 uvicorn server:app --host 0.0.0.0 --port 5001
 
 
