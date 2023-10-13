1e96e0fda750d10904f0d4c3f8af4c0e41be114e
maddadder <rleecharlie@gmail.com>
2023-10-12 17:34:26 -0700

    use a queue

--- generate_server.py
+++ generate_server.py
@@ -8,6 +8,15 @@ from fastapi import FastAPI, Form, Request, Query, HTTPException, status
 from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import json
+import threading
+from pydantic import BaseModel
+from pydub import AudioSegment
+from collections import deque
+import queue
+import time
+import asyncio
+import aio_pika
 
 model = MusicGen.get_pretrained('facebook/musicgen-large', device='cuda')
 model.set_generation_params(duration=12)
@@ -18,6 +27,58 @@ app.mount("/audio", StaticFiles(directory="audio"), name="audio")
 
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
+rabbitmq_connection = None
+rabbitmq_channel = None
+rabbitmq_queue = None
+
+# Connection to RabbitMQ using aio-pika
+async def setup_rabbitmq():
+    global rabbitmq_connection, rabbitmq_channel, rabbitmq_queue
+    rabbitmq_connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+    rabbitmq_channel = await rabbitmq_connection.channel()
+
+    # Declare the queue
+    rabbitmq_queue = await rabbitmq_channel.declare_queue("musicgen_queue", durable=False)
+
+async def callback(ch, message, properties, body):
+    try:
+        text = body.decode("utf-8")
+        start = time.time()
+        wav = model.generate([text], progress=True)
+        wav = wav[0]
+        end = time.time()
+        print(f"Generation took {end - start} seconds")
+
+        save_path = f"audio/{uuid.uuid4()}"
+
+        print(f"Saving {save_path}")
+        audio_write(f'{save_path}', wav.cpu(), model.sample_rate, strategy="loudness")
+        convert_wav_to_mp3(f'{save_path}.wav')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        await message.ack()
+
+
+async def musicgen_worker():
+    global rabbitmq_channel, rabbitmq_queue
+    if rabbitmq_queue is None:
+        print("RabbitMQ queue not initialized.")
+        return
+
+    async with rabbitmq_queue.iterator() as queue_iter:
+        async for message in queue_iter:
+            #async with message.process():
+            await callback(rabbitmq_channel, message, None, message.body)
+
+
+# Get the connection, channel, and queue in the application startup event
+@app.on_event("startup")
+async def startup_event():
+    print('startup_event has occurred')
+    await setup_rabbitmq()
+    # Now that RabbitMQ is initialized, start the worker
+    asyncio.create_task(musicgen_worker())
 
 def convert_wav_to_mp3(wav_path):
     # Determine the MP3 file path
@@ -32,28 +93,26 @@ def convert_wav_to_mp3(wav_path):
     # Delete the WAV file
     os.remove(wav_path)
 
+class MusicGenRequest(BaseModel):
+    text: str
+
 @app.post("/musicgen")
-async def musicgen(text: str = Form(...),response_class=HTMLResponse):
-    try:
-        generated_files = []
-        for i in range(1):  # Generate 1 audio samples
-            start = time.time()
-            wav = model.generate([text], progress=True)
-            wav = wav[0]
-            end = time.time()
-            print(f"Generation took {end - start} seconds")
+async def musicgen(request: MusicGenRequest, response_class=HTMLResponse):
+    global rabbitmq_channel
 
-            save_path = f"audio/{uuid.uuid4()}"
-            generated_files.append(save_path)
-
-            print(f"Saving {save_path}")
-            audio_write(f'{save_path}', wav.cpu(), model.sample_rate, strategy="loudness")
-            convert_wav_to_mp3(f'{save_path}.wav')
-
-        # Redirect to /list_generated_files
+    if rabbitmq_channel is None:
+        print("RabbitMQ channel not initialized.")
         return RedirectResponse("/list_generated_files", status_code=303)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+
+    # Use `await rabbitmq_channel.publish` instead of `rabbitmq_channel.basic_publish`
+    await rabbitmq_channel.default_exchange.publish(
+        aio_pika.Message(body=request.text.encode("utf-8")),
+        routing_key='musicgen_queue'
+    )
+    
+    # Redirect to /list_generated_files
+    return RedirectResponse("/list_generated_files", status_code=303)
+
 
 # HTML template to display results
 @app.get("/results", response_class=HTMLResponse)
--- setup.sh
+++ setup.sh
@@ -9,9 +9,14 @@ pip install git+https://github.com/huggingface/transformers.git
 pip install chardet
 pip install numpy==1.25
 
+# install rabbitmq 
+# with these instructions: https://www.rabbitmq.com/install-debian.html#apt-quick-start-cloudsmith
+# then systemctl restart rabbitmq-server
+pip install aio-pika
+
 #run the following
 conda activate audio
-uvicorn server:app --host 0.0.0.0 --port 5001
+uvicorn generate_server:app --host 0.0.0.0 --port 5001 --reload
 
 
 # open browser to: http://antec.local:5001
