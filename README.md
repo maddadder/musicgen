ba84f600d3faf4164808f3567fb44c92963d6ab2
maddadder <rleecharlie@gmail.com>
2023-10-14 13:08:21 -0700

    update docs

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # musicgen
 
-1. Deploy as a service
+1. Create environment
+```bash
+conda create --name audio python=3.9
+#conda deactivate
+conda activate audio
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+```
+2. test cuda install
+```python
+import torch
+torch.cuda.is_available()
+quit()
+```
+
+3. Continue installing
+```bash
+pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft
+conda install "ffmpeg<5" -c conda-forge
+pip install aio-pika
+pip install pika
+
+# install rabbitmq 
+# with these instructions: https://www.rabbitmq.com/install-debian.html#apt-quick-start-cloudsmith
+# then systemctl restart rabbitmq-server
+
+mkdir audio
+```
+
+4. Deploy as a service
 
 ```bash
 chmod +x app.sh
--- setup.sh
+++ setup.sh
@@ -1,23 +1,9 @@
-# run this setup once
-conda create --name audio python=3.9
-#conda deactivate
-conda activate audio
-conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
-pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft
-conda install "ffmpeg<5" -c conda-forge
-pip install git+https://github.com/huggingface/transformers.git
-pip install chardet
-pip install numpy==1.25
+# See README.md for setup instructions
 
-# install rabbitmq 
-# with these instructions: https://www.rabbitmq.com/install-debian.html#apt-quick-start-cloudsmith
-# then systemctl restart rabbitmq-server
-pip install aio-pika
-
-#run the following
+# To run app
 conda activate audio
 uvicorn generate_server:app --host 0.0.0.0 --port 5001 --reload
 #AND in another terminal
 python consumer.py
 
-# open browser to: http://antec.local:5001
+# open browser to: http://server.local:5001
