13e39a3f0afa004e247709e04da779aa67a0d456
maddadder <rleecharlie@gmail.com>
2023-10-18 16:22:21 -0700

    handle file size limit in blazor

--- demo/Services/FileUploadService.cs
+++ demo/Services/FileUploadService.cs
@@ -2,13 +2,18 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
 
+public static class FileUploadServiceHelper
+{
+    public const long MAXALLOWEDSIZE = 1024000;
+}
+
 public class FileUploadService
 {
     public async Task<string> SaveFile(IBrowserFile file)
     {
         using (var memoryStream = new MemoryStream())
         {
-            await file.OpenReadStream().CopyToAsync(memoryStream);
+            await file.OpenReadStream(FileUploadServiceHelper.MAXALLOWEDSIZE).CopyToAsync(memoryStream);
             var fileContent = memoryStream.ToArray();
             return Convert.ToBase64String(fileContent);
         }
