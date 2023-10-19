1b918abf06c772363f76f1fc688b3cffe30a282b
maddadder <rleecharlie@gmail.com>
2023-10-19 08:12:34 -0700

    show more accurate queue length

--- demo/Services/RabbitMQService.cs
+++ demo/Services/RabbitMQService.cs
@@ -7,7 +7,23 @@ public class RabbitMqService
 {
     private readonly IConnection _connection;
     private readonly IModel _channel;
+    public uint InitialQueueLength 
+    {
+        get
+        {
+            // Get the message count for the queue
+            var queueInfo = _channel.QueueDeclarePassive(standard_queueName);
+            return queueInfo.MessageCount;
+        }
+    }
 
+    private const string irq_exchange = "irq_exchange";
+    private const string irq_routingKey = "irq_key";
+
+    private const string  standard_exchange = "standard_exchange";
+    private const string  standard_routingKey = "standard_key";
+    private const string  standard_queueName = "standard"; 
+    
     public RabbitMqService()
     {
         var factory = new ConnectionFactory
@@ -18,30 +34,31 @@ public class RabbitMqService
 
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
+        _channel.BasicQos(0, 1, false);
+
+        _channel.ExchangeDeclare(exchange: irq_exchange, type: ExchangeType.Direct);
+
+        _channel.ExchangeDeclare(exchange: standard_exchange, type: ExchangeType.Direct);
+        _channel.QueueDeclare(queue: standard_queueName,
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
     }
 
     public void SendInterrupt()
     {
-        var exchangeName = "irq_exchange";
-        var routingKey = "irq_key";
-
-        _channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
-
         var messageBody = Encoding.UTF8.GetBytes("{}");
 
-        _channel.BasicPublish(exchange: exchangeName,
-                              routingKey: routingKey,
+        _channel.BasicPublish(exchange: irq_exchange,
+                              routingKey: irq_routingKey,
                               basicProperties: null,
                               body: messageBody);
     }
 
     public void SendMusicGenRequest(string text, string duration, string audio_file)
     {
-        var exchangeName = "standard_exchange";
-        var routingKey = "standard_key";
-
-        _channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
-
         var request = new MusicGenRequest
         {
             Text = text,
@@ -52,8 +69,8 @@ public class RabbitMqService
         var jsonMessage = JsonSerializer.Serialize(request);
         var messageBody = Encoding.UTF8.GetBytes(jsonMessage);
 
-        _channel.BasicPublish(exchange: exchangeName,
-                              routingKey: routingKey,
+        _channel.BasicPublish(exchange: standard_exchange,
+                              routingKey: standard_routingKey,
                               basicProperties: null,
                               body: messageBody);
     }
